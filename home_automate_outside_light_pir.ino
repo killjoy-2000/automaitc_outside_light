c3466a869b1781b508fd0eec151a6388276f0edb
Killjoy <researchuse@gmail.com>
2022-07-17 19:45:09 +0530

    added time setup

--- home_automate_outside_light_pir.ino
+++ home_automate_outside_light_pir.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#define indicator 13
+#define pir1 5
+#define pir2 6
+#define time_set A5
+#define door 2
+#define light 3
 
+// long time_val;
+long time_on;
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(indicator,OUTPUT);
+  pinMode(pir1,INPUT);
+  pinMode(pir2,INPUT);
+  pinMode(door,INPUT);
+  pinMode(light,OUTPUT);
+  pinMode(time_set,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(indicator,HIGH);
+  Serial.print(analogRead(time_set));
 
+  // time_val = map(analogRead(time_set), 0, 1023, 120, 1200);
+
+  time_on = map(analogRead(time_set), 0, 1023, 120, 1200) * 1000;
+
+  // Serial.print("\t");
+  // Serial.print(time_val);
+  
+  Serial.print("\t");
+  Serial.println(time_on);
 }
