1b7a3731d3e84293c0376bb383e15ebc73226e70
Killjoy <researchuse@gmail.com>
2022-07-21 12:03:45 +0530

    change image and code modified

--- home_automate_outside_light_pir.ino
+++ home_automate_outside_light_pir.ino
@@ -4,9 +4,13 @@
 #define time_set A5
 #define door 2
 #define light 3
+#define ldr A0
 
 // long time_val;
 long time_on;
+int time_org, state = 0;
+unsigned long long prev_time = 0;
+
 
 
 void setup() {
@@ -18,19 +22,49 @@ void setup() {
   pinMode(door,INPUT);
   pinMode(light,OUTPUT);
   pinMode(time_set,INPUT);
+  pinMode(ldr,INPUT);
+
+
+  digitalWrite(indicator,HIGH);
+  
 }
 
-void loop() {
-  digitalWrite(indicator,HIGH);
+void loop() {  
   Serial.print(analogRead(time_set));
+  Serial.print("\t");
+  Serial.print(digitalRead(door));
 
   // time_val = map(analogRead(time_set), 0, 1023, 120, 1200);
 
-  time_on = map(analogRead(time_set), 0, 1023, 120, 1200) * 1000;
+  time_on = map(analogRead(time_set), 0, 1023, 1800, 120) * 1000;
 
   // Serial.print("\t");
   // Serial.print(time_val);
-  
+
   Serial.print("\t");
-  Serial.println(time_on);
+  Serial.print(time_on);
+
+  // view original time 
+  time_org = (time_on/1000)/60;
+  Serial.print("\t");
+  Serial.print(time_org);
+
+  Serial.print("\t");
+  Serial.println(analogRead(ldr));
+
+
+  // main code
+  if(digitalRead(door) == LOW && state ==0 && analogRead(ldr) > 500){
+    digitalWrite(light,HIGH);
+    state = 1;
+  }
+
+  if(state == 1){
+    if(millis() - prev_time >= time_on){
+      digitalWrite(light,LOW);
+      prev_time = millis();
+      state = 0;            
+    }
+  }
+
 }
